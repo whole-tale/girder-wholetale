cbc2229a848eabb201086aa4ec3ce6aa58bf4d28
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2024-07-08 12:28:34 -0500

    port dataset_test to pytest

--- girder_wholetale/tests/dataset_test.py
+++ girder_wholetale/tests/dataset_test.py
@@ -1,136 +1,97 @@
 import json
-import os
-import vcr
-from tests import base
+import pytest
+from pytest_girder.assertions import assertStatusOk
 
 
-DATA_PATH = os.path.join(
-    os.path.dirname(os.environ['GIRDER_TEST_DATA_PREFIX']),
-    'data_src',
-    'plugins',
-    'wholetale',
-)
+@pytest.mark.plugin("wholetale")
+@pytest.mark.vcr()
+def test_dataset_rest(server, admin, user):
+    user_data_map = [
+        {
+            "dataId": "https://dataverse.harvard.edu/dataset.xhtml"
+            "?persistentId=doi:10.7910/DVN/TJCLKP",
+            "doi": "10.7910/DVN/TJCLKP",
+            "name": "Open Source at Harvard",
+            "repository": "Dataverse",
+            "size": 518379,
+            "tale": False,
+        },
+        {
+            "dataId": "http://use.yt/upload/9241131f",
+            "doi": None,
+            "name": "illustris.jpg",
+            "repository": "HTTP",
+            "size": 781665,
+            "tale": False,
+        },
+    ]
 
+    admin_data_map = [
+        {
+            "dataId": (
+                "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/CR02PG"
+            ),
+            "doi": "doi:10.7910/DVN/CR02PG",
+            "name": (
+                "Oral History of the Tenured Women in the Faculty of Arts and Sciences at "
+                "Harvard University, 1981"
+            ),
+            "repository": "Dataverse",
+            "size": 566309963,
+            "tale": False,
+        }
+    ]
 
-def setUpModule():
-    base.enabledPlugins.append('wholetale')
-    base.startServer()
+    resp = server.request(
+        path="/dataset/register",
+        method="POST",
+        params={"dataMap": json.dumps(user_data_map)},
+        user=user,
+    )
+    assertStatusOk(resp)
 
+    resp = server.request(
+        path="/dataset/register",
+        method="POST",
+        params={"dataMap": json.dumps(admin_data_map)},
+        user=admin,
+    )
+    assertStatusOk(resp)
 
-def tearDownModule():
-    base.stopServer()
+    resp = server.request(path="/dataset", method="GET", user=user)
+    assertStatusOk(resp)
+    ds = resp.json
+    assert len(ds) == 3
 
+    resp = server.request(
+        path="/dataset", method="GET", user=user, params={"myData": True}
+    )
+    assertStatusOk(resp)
+    ds = resp.json
+    assert len(ds) == 2
 
-class DatasetTestCase(base.TestCase):
-    def setUp(self):
-        users = (
-            {
-                'email': 'root@dev.null',
-                'login': 'admin',
-                'firstName': 'Root',
-                'lastName': 'van Klompf',
-                'password': 'secret',
-            },
-            {
-                'email': 'joe@dev.null',
-                'login': 'joeregular',
-                'firstName': 'Joe',
-                'lastName': 'Regular',
-                'password': 'secret',
-            },
-        )
-        self.admin, self.user = [
-            self.model('user').createUser(**user) for user in users
-        ]
+    ds = next((_ for _ in ds if _["provider"] == "HTTP"))
+    resp = server.request(
+        path="/dataset/{_id}".format(**ds), method="DELETE", user=user
+    )
+    assertStatusOk(resp)
 
-    @vcr.use_cassette(os.path.join(DATA_PATH, 'dataset_register.txt'))
-    def testDatasetRest(self):
-        user_data_map = [
-            {
-                "dataId": "resource_map_doi:10.5065/D6862DM8",
-                "doi": "10.5065/D6862DM8",
-                "name": "Humans and Hydrology at High Latitudes: Water Use Information",
-                "repository": "DataONE",
-                "size": 28856295,
-            },
-            {
-                "dataId": "http://use.yt/upload/9241131f",
-                "doi": None,
-                "name": "illustris.jpg",
-                "repository": "HTTP",
-                "size": 781665,
-            },
-        ]
+    resp = server.request(
+        path="/dataset", method="GET", user=user, params={"myData": True}
+    )
+    assertStatusOk(resp)
+    ds = resp.json
+    assert len(ds) == 1
 
-        admin_data_map = [
-            {
-                "dataId": "https://dataverse.harvard.edu/dataset.xhtml"
-                "?persistentId=doi:10.7910/DVN/TJCLKP",
-                "doi": "10.7910/DVN/TJCLKP",
-                "name": "Open Source at Harvard",
-                "repository": "Dataverse",
-                "size": 518379,
-            },
-            {
-                "dataId": "https://search-dev.test.dataone.org/view/tao.14232.1",
-                "doi": None,
-                "name": "tao.14232.1",
-                "repository": "HTTP",
-                "size": 6252,
-            },
-        ]
-
-        resp = self.request(
-            path='/dataset/register',
-            method='POST',
-            params={'dataMap': json.dumps(user_data_map)},
-            user=self.user,
-        )
-        self.assertStatusOk(resp)
-
-        resp = self.request(
-            path='/dataset/register',
-            method='POST',
-            params={'dataMap': json.dumps(admin_data_map)},
-            user=self.admin,
-        )
-        self.assertStatusOk(resp)
-
-        resp = self.request(path='/dataset', method='GET', user=self.user)
-        self.assertStatusOk(resp)
-        ds = resp.json
-        self.assertEqual(len(ds), 4)
-
-        resp = self.request(
-            path='/dataset', method='GET', user=self.user, params={'myData': True}
-        )
-        self.assertStatusOk(resp)
-        ds = resp.json
-        self.assertEqual(len(ds), 2)
-
-        resp = self.request(
-            path='/dataset/{_id}'.format(**ds[0]), method='DELETE', user=self.user
-        )
-        self.assertStatusOk(resp)
-
-        resp = self.request(
-            path='/dataset', method='GET', user=self.user, params={'myData': True}
-        )
-        self.assertStatusOk(resp)
-        ds = resp.json
-        self.assertEqual(len(ds), 1)
-
-        resp = self.request(
-            path='/dataset',
-            method='GET',
-            user=self.user,
-            params={
-                'identifiers': json.dumps(
-                    ["urn:uuid:62e1a8c5-406b-43f9-9234-1415277674cb"]
-                )
-            },
-        )
-        self.assertStatusOk(resp)
-        ds = resp.json
-        self.assertEqual(len(ds), 1)
-        self.assertEqual(ds[0]['name'], 'usco2000.xls')
+    resp = server.request(
+        path="/dataset",
+        method="GET",
+        user=user,
+        params={
+            "identifiers": json.dumps(["doi:10.7910/DVN/CR02PG"])
+        },
+    )
+    assertStatusOk(resp)
+    ds = resp.json
+    assert len(ds) == 23  # TODO: Is it correct? It shows all individual files...
+    assert ds[0]["name"].startswith("Oral History")  # Folder will be first anyway
