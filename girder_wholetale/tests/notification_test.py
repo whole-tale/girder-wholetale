71c1c2c8781000c885a941a371a9054a96c25d0b
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2024-07-12 11:28:42 -0500

    port notifications test

--- girder_wholetale/tests/notification_test.py
+++ girder_wholetale/tests/notification_test.py
@@ -1,252 +1,227 @@
-import time
-from tests import base
+import pytest
+from girder.models.notification import Notification, ProgressState
 from girder.models.setting import Setting
+from girder_jobs.constants import JobStatus
+from girder_jobs.models.job import Job
+from girder_worker.girder_plugin.constants import PluginSettings as WorkerPluginSettings
+
+from girder_wholetale.utils import init_progress
 
 
-def setUpModule():
-    base.enabledPlugins.append('wholetale')
-    base.startServer()
+def assertNotification(notification_id, state, total, current, msg):
+    notification = Notification().load(notification_id)
+    assert notification["data"]["state"] == state
+    assert notification["data"]["total"] == total
+    assert notification["data"]["current"] == current
+    assert notification["data"]["message"] == msg
 
 
-def tearDownModule():
-    base.stopServer()
+@pytest.mark.plugin("wholetale")
+def test_single_job_notification(server, user):
+    # TODO: Why do we need it here?
+    Setting().set(WorkerPluginSettings.API_URL, "http://localhost:8080/api/v1")
+
+    total = 2
+    resource = {"type": "wt_test_build_image", "instance_id": "instance_id"}
+
+    notification = init_progress(
+        resource, user, "Test notification", "Creating job", total
+    )
+
+    # Job to test error path
+    job = Job().createJob(
+        title="Error test job",
+        type="test",
+        handler="my_handler",
+        user=user,
+        public=False,
+        args=["tale_id"],
+        kwargs={},
+        otherFields={"wt_notification_id": str(notification["_id"])},
+    )
+    job = Job().updateJob(
+        job, status=JobStatus.INACTIVE, progressTotal=2, progressCurrent=0
+    )
+    assert job["status"] == JobStatus.INACTIVE
+    notification = Notification().load(notification["_id"])
+    assertNotification(notification["_id"], ProgressState.QUEUED, 2, 0, "Creating job")
+    assert notification["data"]["resource"]["jobs"][0] == job["_id"]
+
+    # State change to ACTIVE
+    job = Job().updateJob(job, status=JobStatus.RUNNING)
+    assert job["status"] == JobStatus.RUNNING
+
+    # Progress update
+    job = Job().updateJob(
+        job,
+        status=JobStatus.RUNNING,
+        progressCurrent=1,
+        progressMessage="Error test message",
+    )
+    assertNotification(
+        notification["_id"], ProgressState.ACTIVE, 2, 1, "Error test message"
+    )
+
+    # State change to ERROR
+    job = Job().updateJob(job, status=JobStatus.ERROR)
+    assertNotification(
+        notification["_id"], ProgressState.ERROR, 2, 1, "Error test message"
+    )
+
+    # new notification
+    notification = init_progress(
+        resource, user, "Test notification", "Creating job", total
+    )
+
+    # New job to test success path
+    job = Job().createJob(
+        title="Test Job",
+        type="test",
+        handler="my_handler",
+        user=user,
+        public=False,
+        args=["tale_id"],
+        kwargs={},
+        otherFields={"wt_notification_id": str(notification["_id"])},
+    )
+
+    # State change to ACTIVE
+    job = Job().updateJob(job, status=JobStatus.RUNNING)
+    assert job["status"] == JobStatus.RUNNING
+
+    # Progress update
+    job = Job().updateJob(
+        job,
+        status=JobStatus.RUNNING,
+        progressCurrent=1,
+        progressMessage="Success test message",
+    )
+    assertNotification(
+        notification["_id"], ProgressState.ACTIVE, 2, 1, "Success test message"
+    )
+
+    job = Job().updateJob(job, status=JobStatus.SUCCESS, progressCurrent=2)
+    assertNotification(
+        notification["_id"], ProgressState.SUCCESS, 2, 2, "Success test message"
+    )
 
 
-class NotificationTestCase(base.TestCase):
+@pytest.mark.plugin("wholetale")
+def testChainedJobNotification(server, user):
+    Setting().set(WorkerPluginSettings.API_URL, "http://localhost:8080/api/v1")
 
-    def setUp(self):
-        super(NotificationTestCase, self).setUp()
-        users = ({
-            'email': 'root@dev.null',
-            'login': 'admin',
-            'firstName': 'Root',
-            'lastName': 'van Klompf',
-            'password': 'secret'
-        }, {
-            'email': 'joe@dev.null',
-            'login': 'joeregular',
-            'firstName': 'Joe',
-            'lastName': 'Regular',
-            'password': 'secret'
-        })
+    total = 5  # 2 + 2 + 1
+    resource = {"type": "wt_test_build_image", "instance_id": "instance_id"}
 
-        self.authors = [
-            {
-                'firstName': 'Charles',
-                'lastName': 'Darwmin',
-                'orcid': 'https://orcid.org/000-000'
-            },
-            {
-                'firstName': 'Thomas',
-                'lastName': 'Edison',
-                'orcid': 'https://orcid.org/111-111'
-            }
-        ]
-        self.admin, self.user = [self.model('user').createUser(**user)
-                                 for user in users]
+    notification = init_progress(
+        resource, user, "Test notification", "Creating job", total
+    )
 
-    def testSingleJobNotification(self):
-        from girder.plugins.jobs.models.job import Job
-        from girder.plugins.jobs.constants import JobStatus
-        from girder.models.notification import Notification, ProgressState
-        from girder.plugins.wholetale.utils import init_progress
-        from girder.plugins.worker.constants import PluginSettings as WorkerPluginSettings
+    # First Job
+    job = Job().createJob(
+        title="First Job",
+        type="test",
+        handler="my_handler",
+        user=user,
+        public=False,
+        args=["tale_id"],
+        kwargs={},
+        otherFields={"wt_notification_id": str(notification["_id"])},
+    )
+    # State change to ACTIVE
+    msg = "Some message 1"
+    job = Job().updateJob(
+        job,
+        status=JobStatus.RUNNING,
+        progressCurrent=1,
+        progressTotal=2,
+        progressMessage=msg,
+    )
+    assert job["status"] == JobStatus.RUNNING
+    assertNotification(notification["_id"], ProgressState.ACTIVE, total, 1, msg)
 
-        # TODO: Why do we need it here?
-        Setting().set(WorkerPluginSettings.API_URL, 'http://localhost:8080/api/v1')
+    msg = "Success msg 1"
+    job = Job().updateJob(
+        job,
+        status=JobStatus.SUCCESS,
+        progressCurrent=2,
+        progressTotal=2,
+        progressMessage=msg,
+    )
+    assert job["status"] == JobStatus.SUCCESS
+    assertNotification(notification["_id"], ProgressState.ACTIVE, total, 2, msg)
 
-        total = 2
-        resource = {'type': 'wt_test_build_image', 'instance_id': 'instance_id'}
+    # Second Job
+    job = Job().createJob(
+        title="Second Job",
+        type="test",
+        handler="my_handler",
+        user=user,
+        public=False,
+        args=["tale_id"],
+        kwargs={},
+        otherFields={"wt_notification_id": str(notification["_id"])},
+    )
+    # State change to QUEUE
+    msg = "Some message 1"
+    job = Job().updateJob(
+        job,
+        status=JobStatus.INACTIVE,
+        progressTotal=2,
+        progressCurrent=0,
+        progressMessage=msg,
+    )
+    assert job["status"] == JobStatus.INACTIVE
+    assertNotification(notification["_id"], ProgressState.QUEUED, total, 2, msg)
 
-        notification = init_progress(
-            resource, self.user, 'Test notification', 'Creating job', total
-        )
+    job = Job().updateJob(job, status=JobStatus.QUEUED)
+    assertNotification(notification["_id"], ProgressState.QUEUED, total, 2, msg)
 
-        # Job to test error path
-        job = Job().createJob(
-            title='Error test job',
-            type='test',
-            handler='my_handler',
-            user=self.user,
-            public=False,
-            args=["tale_id"],
-            kwargs={},
-            otherFields={'wt_notification_id': str(notification['_id'])},
-        )
-        job = Job().updateJob(
-            job, status=JobStatus.INACTIVE, progressTotal=2, progressCurrent=0)
-        self.assertEqual(job['status'], JobStatus.INACTIVE)
-        notification = Notification().load(notification['_id'])
-        self.assertEqual(notification['data']['state'], ProgressState.QUEUED)
-        self.assertEqual(notification['data']['total'], 2)
-        self.assertEqual(notification['data']['current'], 0)
-        self.assertEqual(notification['data']['resource']['jobs'][0], job['_id'])
+    job = Job().updateJob(job, status=JobStatus.RUNNING)
+    assertNotification(notification["_id"], ProgressState.ACTIVE, total, 2, msg)
 
-        # State change to ACTIVE
-        job = Job().updateJob(job, status=JobStatus.RUNNING)
-        self.assertEqual(job['status'], JobStatus.RUNNING)
+    msg = "Success msg 2"
+    job = Job().updateJob(
+        job,
+        status=JobStatus.SUCCESS,
+        progressCurrent=2,
+        progressTotal=2,
+        progressMessage=msg,
+    )
+    assert job["status"] == JobStatus.SUCCESS
+    assertNotification(notification["_id"], ProgressState.ACTIVE, total, 4, msg)
 
-        # Progress update
-        job = Job().updateJob(
-            job, status=JobStatus.RUNNING, progressCurrent=1,
-            progressMessage="Error test message")
-        notification = Notification().load(notification['_id'])
-        self.assertEqual(notification['data']['state'], ProgressState.ACTIVE)
-        self.assertEqual(notification['data']['total'], 2)
-        self.assertEqual(notification['data']['current'], 1)
-        self.assertEqual(notification['data']['message'], 'Error test message')
+    # Final Job
+    job = Job().createJob(
+        title="Final Job",
+        type="test",
+        handler="my_handler",
+        user=user,
+        public=False,
+        args=["tale_id"],
+        kwargs={},
+        otherFields={"wt_notification_id": str(notification["_id"])},
+    )
+    msg = "Some message 1"
+    job = Job().updateJob(
+        job,
+        status=JobStatus.INACTIVE,
+        progressTotal=1,
+        progressCurrent=0,
+        progressMessage=msg,
+    )
+    assert job["status"] == JobStatus.INACTIVE
+    assertNotification(notification["_id"], ProgressState.QUEUED, total, 4, msg)
 
-        # State change to ERROR
-        job = Job().updateJob(job, status=JobStatus.ERROR)
-        notification = Notification().load(notification['_id'])
-        self.assertEqual(notification['data']['state'], ProgressState.ERROR)
-        self.assertEqual(notification['data']['total'], 2)
-        self.assertEqual(notification['data']['current'], 1)
-        self.assertEqual(notification['data']['message'], 'Error test message')
-
-        # new notification
-        notification = init_progress(
-            resource, self.user, 'Test notification', 'Creating job', total
-        )
-
-        # New job to test success path
-        job = Job().createJob(
-            title='Test Job',
-            type='test',
-            handler='my_handler',
-            user=self.user,
-            public=False,
-            args=["tale_id"],
-            kwargs={},
-            otherFields={'wt_notification_id': str(notification['_id'])},
-        )
-
-        # State change to ACTIVE
-        job = Job().updateJob(job, status=JobStatus.RUNNING)
-        self.assertEqual(job['status'], JobStatus.RUNNING)
-
-        # Progress update
-        job = Job().updateJob(
-            job, status=JobStatus.RUNNING, progressCurrent=1,
-            progressMessage="Success test message")
-        notification = Notification().load(notification['_id'])
-        self.assertEqual(notification['data']['state'], ProgressState.ACTIVE)
-        self.assertEqual(notification['data']['total'], 2)
-        self.assertEqual(notification['data']['current'], 1)
-        self.assertEqual(notification['data']['message'], 'Success test message')
-
-        job = Job().updateJob(job, status=JobStatus.SUCCESS, progressCurrent=2)
-        notification = Notification().load(notification['_id'])
-        self.assertEqual(notification['data']['state'], ProgressState.SUCCESS)
-        self.assertEqual(notification['data']['total'], 2)
-        self.assertEqual(notification['data']['current'], 2)
-        self.assertEqual(notification['data']['message'], 'Success test message')
-
-    def assertNotification(self, notification_id, state, total, current, msg):
-        from girder.models.notification import Notification
-        notification = Notification().load(notification_id)
-        self.assertEqual(notification['data']['state'], state)
-        self.assertEqual(notification['data']['total'], total)
-        self.assertEqual(notification['data']['current'], current)
-        self.assertEqual(notification['data']['message'], msg)
-
-    def testChainedJobNotification(self):
-        from girder.plugins.jobs.models.job import Job
-        from girder.plugins.jobs.constants import JobStatus
-        from girder.models.notification import Notification, ProgressState
-        from girder.plugins.wholetale.utils import init_progress
-        from girder.plugins.worker.constants import PluginSettings as WorkerPluginSettings
-
-        Setting().set(WorkerPluginSettings.API_URL, 'http://localhost:8080/api/v1')
-
-        total = 5 # 2 + 2 + 1
-        resource = {'type': 'wt_test_build_image', 'instance_id': 'instance_id'}
-
-        notification = init_progress(
-            resource, self.user, 'Test notification', 'Creating job', total
-        )
-
-        # First Job
-        job = Job().createJob(
-            title='First Job',
-            type='test',
-            handler='my_handler',
-            user=self.user,
-            public=False,
-            args=["tale_id"],
-            kwargs={},
-            otherFields={'wt_notification_id': str(notification['_id'])},
-        )
-        # State change to ACTIVE
-        msg = "Some message 1"
-        job = Job().updateJob(
-            job, status=JobStatus.RUNNING, progressCurrent=1, progressTotal=2,
-            progressMessage=msg)
-        self.assertEqual(job['status'], JobStatus.RUNNING)
-        self.assertNotification(notification["_id"], ProgressState.ACTIVE, total, 1, msg)
-
-        msg = "Success msg 1"
-        job = Job().updateJob(
-            job, status=JobStatus.SUCCESS, progressCurrent=2, progressTotal=2,
-            progressMessage=msg)
-        self.assertEqual(job['status'], JobStatus.SUCCESS)
-        self.assertNotification(notification["_id"], ProgressState.ACTIVE, total, 2, msg)
-
-        # Second Job
-        job = Job().createJob(
-            title='Second Job',
-            type='test',
-            handler='my_handler',
-            user=self.user,
-            public=False,
-            args=["tale_id"],
-            kwargs={},
-            otherFields={'wt_notification_id': str(notification['_id'])},
-        )
-        # State change to QUEUE
-        msg = "Some message 1"
-        job = Job().updateJob(
-            job, status=JobStatus.INACTIVE, progressTotal=2, progressCurrent=0,
-            progressMessage=msg)
-        self.assertEqual(job['status'], JobStatus.INACTIVE)
-        self.assertNotification(notification["_id"], ProgressState.QUEUED, total, 2, msg)
-
-        job = Job().updateJob(job, status=JobStatus.QUEUED)
-        self.assertNotification(notification["_id"], ProgressState.QUEUED, total, 2, msg)
-
-        job = Job().updateJob(job, status=JobStatus.RUNNING)
-        self.assertNotification(notification["_id"], ProgressState.ACTIVE, total, 2, msg)
-
-        msg = "Success msg 2"
-        job = Job().updateJob(
-            job, status=JobStatus.SUCCESS, progressCurrent=2, progressTotal=2,
-            progressMessage=msg)
-        self.assertEqual(job['status'], JobStatus.SUCCESS)
-        self.assertNotification(notification["_id"], ProgressState.ACTIVE, total, 4, msg)
-
-        # Final Job
-        job = Job().createJob(
-            title='Final Job',
-            type='test',
-            handler='my_handler',
-            user=self.user,
-            public=False,
-            args=["tale_id"],
-            kwargs={},
-            otherFields={'wt_notification_id': str(notification['_id'])},
-        )
-        msg = "Some message 1"
-        job = Job().updateJob(
-            job, status=JobStatus.INACTIVE, progressTotal=1, progressCurrent=0,
-            progressMessage=msg)
-        self.assertEqual(job['status'], JobStatus.INACTIVE)
-        self.assertNotification(notification["_id"], ProgressState.QUEUED, total, 4, msg)
-
-        job = Job().updateJob(job, status=JobStatus.QUEUED)
-        job = Job().updateJob(job, status=JobStatus.RUNNING)
-        msg = "Success msg 3"
-        job = Job().updateJob(
-            job, status=JobStatus.SUCCESS, progressCurrent=1, progressTotal=1,
-            progressMessage=msg)
-        self.assertEqual(job['status'], JobStatus.SUCCESS)
-        self.assertNotification(notification["_id"], ProgressState.SUCCESS, total, 5, msg)
+    job = Job().updateJob(job, status=JobStatus.QUEUED)
+    job = Job().updateJob(job, status=JobStatus.RUNNING)
+    msg = "Success msg 3"
+    job = Job().updateJob(
+        job,
+        status=JobStatus.SUCCESS,
+        progressCurrent=1,
+        progressTotal=1,
+        progressMessage=msg,
+    )
+    assert job["status"] == JobStatus.SUCCESS
+    assertNotification(notification["_id"], ProgressState.SUCCESS, total, 5, msg)
