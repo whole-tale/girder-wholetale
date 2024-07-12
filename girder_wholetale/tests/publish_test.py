c4d8250f60edd1a4fcc42a225e207b2af6d9a893
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2024-07-12 11:39:24 -0500

    port more tests

--- girder_wholetale/tests/publish_test.py
+++ girder_wholetale/tests/publish_test.py
@@ -1,205 +1,121 @@
 import json
+
 import mock
-from tests import base
-
+import pytest
 from girder.models.user import User
+from pytest_girder.assertions import assertStatus, assertStatusOk
+
+from girder_wholetale.constants import PluginSettings, SettingDefault
+from girder_wholetale.models.tale import Tale
 
 
-def setUpModule():
-    base.enabledPlugins.append("wholetale")
-    base.enabledPlugins.append("wt_home_dir")
-    base.enabledPlugins.append("wt_versioning")
-    base.startServer()
+@pytest.fixture
+def mock_builder(mocker):
+    mock_builder = mocker.patch("girder_wholetale.lib.manifest.ImageBuilder")
+    mock_builder.return_value.container_config.repo2docker_version = (
+        "craigwillis/repo2docker:latest"
+    )
+    mock_builder.return_value.get_tag.return_value = (
+        "images.local.wholetale.org/digest123"
+    )
+    return mock_builder
 
-    global JobStatus, Tale
-    from girder.plugins.jobs.constants import JobStatus
-    from girder.plugins.wholetale.models.tale import Tale
 
-
-def tearDownModule():
-    base.stopServer()
+@pytest.fixture
+def simple_tale(image, user):
+    tale = Tale().createTale(
+        image,
+        [],
+        creator=user,
+        save=True,
+        title="Some tale with dataset and versions",
+        description="Something something...",
+        public=False,
+        icon="https://icon-picture.com/icon.png",
+        config={},
+        authors=[
+            {
+                "firstName": "Kacper",
+                "lastName": "Kowalik",
+                "orcid": "https://orcid.org/0000-0003-1709-3744",
+            }
+        ],
+        category="science",
+    )
+    yield tale
+    Tale().remove(tale)
 
 
 class FakeJob:
-
     job = {}
 
     def delay(self, *args, **kwargs):
         return self.job
 
 
-class PublishTestCase(base.TestCase):
-    def setUp(self):
-        super(PublishTestCase, self).setUp()
-        users = (
-            {
-                "email": "root@dev.null",
-                "login": "admin",
-                "firstName": "Root",
-                "lastName": "van Klompf",
-                "password": "secret",
-            },
-            {
-                "email": "joe@dev.null",
-                "login": "joeregular",
-                "firstName": "Joe",
-                "lastName": "Regular",
-                "password": "secret",
-            },
-        )
-        self.admin, self.user = [User().createUser(**user) for user in users]
-        self.image = self.model("image", "wholetale").createImage(
-            name="test my name", creator=self.admin, public=True,
-            config=dict(template='base.tpl', buildpack='SomeBuildPack',
-                        user='someUser', port=8888, urlPath='')
-        )
+@pytest.mark.plugin("wholetale")
+def test_config_validators(server, admin):
+    resp = server.request(
+        "/system/setting",
+        user=admin,
+        method="PUT",
+        params={"key": PluginSettings.PUBLISHER_REPOS, "value": "random_string"},
+    )
+    assertStatus(resp, 400)
+    assert resp.json["message"].startswith("Invalid Repository to Auth Provider map")
 
-        self.tale = self.model("tale", "wholetale").createTale(
-            self.image,
-            data=[],
-            authors=self.user["firstName"] + " " + self.user["lastName"],
-            creator=self.user,
-            public=True,
-            description="blah",
-            title="Test",
-            illustration="linkToImage",
-        )
+    resp = server.request(
+        "/system/setting",
+        user=admin,
+        method="PUT",
+        params={
+            "key": PluginSettings.PUBLISHER_REPOS,
+            "value": json.dumps(
+                SettingDefault.defaults[PluginSettings.PUBLISHER_REPOS]
+            ),
+        },
+    )
+    assertStatusOk(resp)
 
-    def testConfigValidators(self):
-        from girder.plugins.wholetale.constants import PluginSettings, SettingDefault
 
-        resp = self.request(
-            "/system/setting",
-            user=self.admin,
+@pytest.mark.plugin("wholetale")
+def test_publish_zenodo(server, user, simple_tale, mock_builder):
+    tale = simple_tale
+    with mock.patch("gwvolman.tasks.publish.apply_async"), mock.patch(
+        "gwvolman.tasks.publish.delay"
+    ) as dl:
+        dl.return_value = FakeJob()
+
+        repository = "sandbox.zenodo.org"
+        resp = server.request(
+            path="/tale/{_id}/publish".format(**tale),
             method="PUT",
-            params={"key": PluginSettings.PUBLISHER_REPOS, "value": "random_string"},
+            user=user,
+            params={"repository": repository},
         )
-        self.assertStatus(resp, 400)
-        self.assertTrue(
-            resp.json["message"].startswith("Invalid Repository to Auth Provider map")
-        )
+        assertStatus(resp, 400)
+        assert resp.json["message"] == "Missing a token for publisher (zenodo)."
 
-        resp = self.request(
-            "/system/setting",
-            user=self.admin,
+        token = {
+            "access_token": "zenodo_key",
+            "provider": "zenodod",
+            "resource_server": "sandbox.zenodo.org",
+            "token_type": "apikey",
+        }
+        user["otherTokens"] = [token]
+        user = User().save(user)
+
+        resp = server.request(
+            path="/tale/{_id}/publish".format(**tale),
             method="PUT",
-            params={
-                "key": PluginSettings.PUBLISHER_REPOS,
-                "value": json.dumps(
-                    SettingDefault.defaults[PluginSettings.PUBLISHER_REPOS]
-                ),
-            },
+            user=user,
+            params={"repository": repository},
         )
-        self.assertStatusOk(resp)
+        assertStatusOk(resp)
 
-    def testPublishDataONE(self):
-        with mock.patch("gwvolman.tasks.publish.apply_async"), mock.patch(
-            "gwvolman.tasks.publish.delay"
-        ) as dl, mock.patch(
-            "girder.plugins.wholetale.lib.manifest.ImageBuilder"
-        ) as mock_builder:
-            mock_builder.return_value.container_config.repo2docker_version = \
-                "craigwillis/repo2docker:latest"
-            mock_builder.return_value.get_tag.return_value = "images.local.wholetale.org/digest123"
-            dl.return_value = FakeJob()
-
-            remoteMemberNode = "nowhere"  # non exisiting repository
-            resp = self.request(
-                path="/tale/{_id}/publish".format(**self.tale),
-                method="PUT",
-                user=self.user,
-                params={"repository": remoteMemberNode},
-            )
-            self.assertStatus(resp, 400)
-            self.assertEqual(
-                resp.json["message"], "Unknown publisher repository (nowhere)"
-            )
-
-            remoteMemberNode = "https://dev.nceas.ucsb.edu/knb/d1/mn"
-            resp = self.request(
-                path="/tale/{_id}/publish".format(**self.tale),
-                method="PUT",
-                user=self.user,
-                params={"repository": remoteMemberNode},
-            )
-            self.assertStatus(resp, 400)
-            self.assertEqual(
-                resp.json["message"], "Missing a token for publisher (dataone)."
-            )
-
-            # "Authenticate" with DataONE
-            token = {
-                "access_token": "dataone_token",
-                "provider": "dataone",
-                "resource_server": "cn-stage.dataone.org",
-                "token_type": "apikey",
-            }
-            self.user["otherTokens"] = [token]
-            self.user = User().save(self.user)
-
-            resp = self.request(
-                path="/tale/{_id}/publish".format(**self.tale),
-                method="PUT",
-                user=self.user,
-                params={"repository": remoteMemberNode},
-            )
-            self.assertStatusOk(resp)
-
-            job_kwargs = dl.call_args_list[-1][1]
-            job_args = dl.call_args_list[-1][0]
-            self.assertEqual(job_args[0], str(self.tale["_id"]))
-            self.assertDictEqual(job_args[1], token)
-            self.assertTrue(job_args[2] is not None)
-            self.assertEqual(job_kwargs["repository"], remoteMemberNode)
-
-    def testPublishZenodo(self):
-        with mock.patch("gwvolman.tasks.publish.apply_async"), mock.patch(
-            "gwvolman.tasks.publish.delay"
-        ) as dl, mock.patch(
-            "girder.plugins.wholetale.lib.manifest.ImageBuilder"
-        ) as mock_builder:
-            mock_builder.return_value.container_config.repo2docker_version = \
-                "craigwillis/repo2docker:latest"
-            mock_builder.return_value.get_tag.return_value = "images.local.wholetale.org/digest123"
-            dl.return_value = FakeJob()
-
-            repository = "sandbox.zenodo.org"
-            resp = self.request(
-                path="/tale/{_id}/publish".format(**self.tale),
-                method="PUT",
-                user=self.user,
-                params={"repository": repository},
-            )
-            self.assertStatus(resp, 400)
-            self.assertEqual(
-                resp.json["message"], "Missing a token for publisher (zenodo)."
-            )
-
-            token = {
-                "access_token": "zenodo_key",
-                "provider": "zenodod",
-                "resource_server": "sandbox.zenodo.org",
-                "token_type": "apikey",
-            }
-            self.user["otherTokens"] = [token]
-            self.user = User().save(self.user)
-
-            resp = self.request(
-                path="/tale/{_id}/publish".format(**self.tale),
-                method="PUT",
-                user=self.user,
-                params={"repository": repository},
-            )
-            self.assertStatusOk(resp)
-
-            job_kwargs = dl.call_args_list[-1][1]
-            job_args = dl.call_args_list[-1][0]
-            self.assertEqual(job_args[0], str(self.tale["_id"]))
-            self.assertDictEqual(job_args[1], token)
-            self.assertTrue(job_args[2] is not None)
-            self.assertEqual(job_kwargs["repository"], repository)
-
-    def tearDown(self):
-        User().remove(self.user)
-        User().remove(self.admin)
-        super(PublishTestCase, self).tearDown()
+        job_kwargs = dl.call_args_list[-1][1]
+        job_args = dl.call_args_list[-1][0]
+        assert job_args[0] == str(tale["_id"])
+        assert job_args[1] == token
+        assert job_args[2] is not None
+        assert job_kwargs["repository"] == repository
--- girder_wholetale/tests/utils_test.py
+++ girder_wholetale/tests/utils_test.py
@@ -1,24 +1,12 @@
-from tests import base
+import pytest
+from girder_wholetale.utils import getOrCreateRootFolder
 
 
-def setUpModule():
+@pytest.mark.plugin("wholetale")
+def test_getOrCreateRootFolder(server):
+    folder_name = "folder_name"
+    folder_desc = "folder_description"
+    folder = getOrCreateRootFolder(folder_name, folder_desc)
 
-    base.enabledPlugins.append('wholetale')
-    base.startServer()
-
-
-def tearDownModule():
-
-    base.stopServer()
-
-
-class TestUtils(base.TestCase):
-
-    def test_getOrCreateRootFolder(self):
-        from server.utils import getOrCreateRootFolder
-        folder_name = 'folder_name'
-        folder_desc = 'folder_description'
-        folder = getOrCreateRootFolder(folder_name, folder_desc)
-
-        self.assertEqual(folder['name'], folder_name)
-        self.assertEqual(folder['description'], folder_desc)
+    assert folder["name"] == folder_name
+    assert folder["description"] == folder_desc
