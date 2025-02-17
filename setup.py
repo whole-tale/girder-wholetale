9863845968fb9a20330b552b8152be3683500d1b
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2025-02-17 07:56:39 -0600

    fix: use the correct apiRoot in footer link

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name="girder-wholetale",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="2.0.1",
+    version="2.0.2",
     description="Girder plugin implementing Whole Tale core functionality.",
     packages=find_packages(),
     include_package_data=True,
